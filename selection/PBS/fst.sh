525a687ddf0cc74ae4e7a62de9204ab07a24a8b8
Rabia Fidan <f.rabiafidan@gmail.com>
2022-02-09 12:29:23 +0300

    Update fst.sh

--- selection/PBS/fst.sh
+++ selection/PBS/fst.sh
@@ -9,7 +9,7 @@
 echo "SLURM_NODELIST $SLURM_NODELIST"
 echo "NUMBER OF CORES $SLURM_NTASKS"
 
-for i in africa eas eurasia; do	cut -f 1 {i}.tsv | awk 'FNR>1 {print$0}'>{i}.txt; done
+for i in africa eas eurasia; do	cut -f 1 ${i}.tsv | awk 'FNR>1 {print$0}'>${i}.txt; done
 
 /usr/local/sw/vcftools-0.1.16/bin/vcftools --gzvcf chr2.1000genome.phase3.v5a.ALL.filtered.recode.vcf.gz --weir-fst-pop eas.txt --weir-fst-pop eurasia.txt --fst-window-size 10000 --fst-window-step 5000 --out eas_euas_norep --exclude-bed chr2_sorted_rep.bed
 
