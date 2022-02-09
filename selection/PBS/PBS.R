f0a171c6caebe25969f803dc0966d8b4a89bd772
rabia <f.rabiafidan@gmail.com>
2022-02-09 12:54:25 +0300

    harika commit mesajı

--- selection/PBS/PBS.R
+++ selection/PBS/PBS.R
@@ -4,11 +4,11 @@ af_euas=read.delim("af_euas_norep.windowed.weir.fst")
 af_euas=af_euas[,c(2,4,5)]
 colnames(af_euas)=c("POS","n_var","af_euas")
 
-af_eas=read.delim("af_eas_norep_windowed.weir.fst")
+af_eas=read.delim("af_eas_norep.windowed.weir.fst")
 af_eas=af_eas[,c(2,4,5)]
 colnames(af_eas)=c("POS","n_var","af_eas")
 
-eas_euas=read.delim("eas_euas_norep_windowed.weir.fst")
+eas_euas=read.delim("eas_euas_norep.windowed.weir.fst")
 eas_euas=eas_euas[,c(2,4,5)]
 colnames(eas_euas)=c("POS","n_var","eas_euas")
 
@@ -22,7 +22,7 @@ t_Fst=-log10(1-Fst)
 
 merged$PBS=(t_Fst$af_eas+t_Fst$eas_euas-t_Fst$af_euas)/2
 
-jpeg("plot.jpeg",width = 1000,height = 500)
+pdf("plot.pdf",height=5,width=8)
 plot(merged$PBS~merged$POS,pch=20,col="gray45",xlab="POS",ylab="PBS",las=1)
 abline(h=mean(merged$PBS)+3*sqrt(var(merged$PBS)))
 dev.off()
---
+++ selection/PBS/README.md
@@ -0,0 +1,7 @@
+## Population Branch Statistics
+
+[presentation](https://docs.google.com/presentation/d/1nZUivOes9ja9d0N8ALRksRTiFYbWmcIE_smXtj_i6x0/edit?usp=sharing)
+
+[uscs table browser](https://genome.ucsc.edu/cgi-bin/hgTables)
+
+
